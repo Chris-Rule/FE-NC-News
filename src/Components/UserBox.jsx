a431641f0b6eed34ace86c23752b5a40946e3257
Chris-Rule <rule-@hotmail.com>
2022-08-26 10:00:01 +0100

    user context added

--- src/Components/UserBox.jsx
+++ src/Components/UserBox.jsx
@@ -1,8 +1,12 @@
+import { useContext } from 'react';
+import { UserContext } from '../Contexts/userContext';
 import './Component-Style/HeaderBar.css'
 
 const UserBox = () => {
+    const{activeUser, setActiveUser} = useContext(UserContext);
+
     return <button className="userBox">
-        User
+       <p>User:</p>  <sub>{activeUser}</sub>
     </button>
 };
 
